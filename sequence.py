e5d82b26c2445626e282d4c8addf535b2ebae2ab
qinrui <martin.qinrui@gmail.com>
2012-02-24 21:35:22 +0800

    demo

---
+++ sequence.py
@@ -0,0 +1,18 @@
+#encoding=utf8
+import random
+
+l1 = []
+
+while len(l1) < 100000:
+    l1.append(random.randrange(-10000000, 1000000)
+
+def find():
+'''
+    add your code
+'''
+    pass
+
+if __name__ == '__main__':
+    find()
+
+
