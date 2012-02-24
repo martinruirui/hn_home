ff7eaa0c5d82214e2627c3696cd6b41c7037f0af
朱 佳佳 <jiajia@akematoMacBook-Pro.local>
2012-02-24 22:10:37 +0800

    Python demo

--- sequence.py
+++ sequence.py
@@ -10,8 +10,13 @@ def find():
 '''
     add your code
 '''
-    pass
-
+    count = 0;   
+    for i in range(len(l1)):
+	    for j in range(1,len(l1)):
+		    if l1[i] + l1[j] == 0:
+			    count++
+			    print l1[i] + '+' l1[j] + '=0'
+    count = count / 2
 if __name__ == '__main__':
     find()
 
